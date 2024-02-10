4bae24bfe1060b21887a6d7c7332ddb040beb6f5
Betul <betulkoru06@gmail.com>
2024-02-10 23:21:36 +0300

    PlayerCard.jsx final

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -5,17 +5,42 @@ import { Card } from "react-bootstrap";
 const PlayerCard = ({img, name, statistics}) => {
     const [showImage, setshowImage] = useState(true)
     console.log(showImage);
+    const handleClick = () => setshowImage(!showImage)
   return (
-    <Card className="rounded-2 m-auto player-card">
+    <Card 
+    className="rounded-2 m-auto player-card" 
+    role="button" 
+    onClick={handleClick}
+    // onClick = {() => setShowImage(!showImage)}
+    >
       { showImage ? (
         <Card.Img variant="top" src={img} className="player-logo" />
       ) : (
         <ul className="m-auto">
             {statistics.map((item,i) => (
-                <li className="list-unstyled h5 text-start" key={i}> 🏀 {item}</li>
+                <li className="list-unstyled h5 text-start" key={i}> 
+                {""}
+                🏀 {item}
+                </li>
             ))}
         </ul>
       )}
+
+    {/* short circuit method */}
+    {/* {showImage && (
+        <Card.Img variant="top" src={img} className="player-logo" />
+    )}
+    {!showImage && (
+    <ul className="m-auto">
+        {statistics.map((item, i) => (
+        <li className="list-unstyled h5 text-start " key={i}>
+            {" "}
+        🏀 {item}
+        </li>
+        ))}
+    </ul>
+    )} */}
+
         <Card.Footer>
             <Card.Title>{name}</Card.Title>
         </Card.Footer>
