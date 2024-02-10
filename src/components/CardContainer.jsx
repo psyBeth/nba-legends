68c9e35784b27dae6f29037c70d4cfaa5060e44a
Betul <betulkoru06@gmail.com>
2024-02-10 23:47:38 +0300

    search bar useState

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,21 +1,33 @@
 import { Card, Container, Form } from "react-bootstrap";
 import Row  from "react-bootstrap/Row";
 import { data } from "../helpers/data";
+
 import { Col } from "react-bootstrap/esm";
 import PlayerCard from "./PlayerCard";
+import { useState } from "react";
 
 const CardContainer = () => {
-    console.log(data);
-    // const showImage = true;
+    const [search,setSearch] = useState("")
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+        //! setter methods run asynchronously. We cannot instantly access the updated state within the function.
+        // console.log(search, "intro");
+    }
+    console.log(search, "extro");
+
+    const filteredData = data.filter(item => 
+        item.name.toLowerCase().includes(search.toLowerCase().trim()));
   return (
     <>
     <Form.Control 
         placeholder = "Search a player"
-        className="w-50 m-auto"/> 
+        className="w-50 m-auto"
+        onChange={handleChange}
+        // onChange={(e) => console.log(e.target.value)}
+        /> 
         <Container className="rounded-4 my-4 p-3 card-container">
             <Row className="justify-content-center g-3">
-                {data.map((player, i) => (
-                // console.log(player)
+                {/* {data.map((player, i) => (
                 <Col x1={3} lg={4} md={6} key={i}>
                     {/* <Card>
                         <Card.Img variant="top" src={player.img} />
@@ -23,9 +35,14 @@ const CardContainer = () => {
                             <Card.Title>{player.name}</Card.Title>
                         </Card.Footer>
                     </Card> */}
-                    <PlayerCard {...player} />
-                </Col>
+                    {/* <PlayerCard {...player} />
+                </Col> */}
+                {filteredData.map((player, i) => (
+                    <Col xl={3} lg={4} md={6} key={i}>
+                        <PlayerCard {...player} />
+                    </Col>
                 ))}
+                
             </Row>
         </Container>
     </>
